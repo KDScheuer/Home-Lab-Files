e52ebeca52f331e4a76f15a563e5d36ef8c874d5
Kristopher Scheuer <143353047+KDScheuer@users.noreply.github.com>
2024-04-28 22:13:59 -0600

    Update after_all_up.sh

--- after_all_up.sh
+++ after_all_up.sh
@@ -1,36 +1,26 @@
 #!/bin/bash
 
-(sleep 420 & {
+(sleep 60 & {
   # Update was done during provisioning this upgrades and installs pip
   sudo apt upgrade -y
   sudo apt-get install python3-pip -y
 
-  # Copys all public keys into ~/.ssh and puts them as authorized_keys
-  retry_copy_and_append_ssh_keys() {
-  local retries=5
-  local success=false
-  for i in {1..3}; do
-    key="/vagrant/keys/ubuntu${i}_id_rsa.pub"
-    while [ "$retries" -gt 0 ]; do
-      if sudo cp "$key" /home/vagrant/.ssh/ && sudo cat "$key" >> /home/vagrant/.ssh/authorized_keys; then
-        success=true
-        break
-      else
-        ((retries--))
-        sleep 30
-      fi
-    done
-}
-
-# Call the function
-retry_copy_and_append_ssh_keys
-
-  # Installs Ansible and Copies Files from Git
-  if [ "$(hostname)" = "ubuntu3" ]; then
+  # Sleeping to Allow Time for Other Boxes to Become Up and Place Pub Key in Shared Folder
+  if [ "$(hostname)" = "ubuntu1" ]; then
+    sleep 420
+  elif [ "$(hostname)" = "ubuntu2" ]; then
+    sleep 360
+  elif [ "$(hostname)" = "ubuntu3" ]; then
     sleep 30
-    sudo apt install ansible -y 2>> ansible-install-log.txt
-    git clone https://github.com/KDScheuer/Home-Lab-Files.git 2>> git-clone-log.txt
-    sleep 10
-    ansible-playbook /home/vagrant/Home-Lab-Files/Ansible_Files/inital_env_setup.yml -i /home/vagrant/Home-Lab-Files/Ansible_Files/inventory >> env-setup-results.txt
+    sudo apt install ansible -y
+    git clone https://github.com/KDScheuer/Home-Lab-Files.git
   fi
+
+  # Copys all public keys into ~/.ssh and puts them as authorized_keys
+  sudo cp /vagrant/keys/ubuntu1_id_rsa.pub /home/vagrant/.ssh/
+  sudo cp /vagrant/keys/ubuntu2_id_rsa.pub /home/vagrant/.ssh/
+  sudo cp /vagrant/keys/ubuntu3_id_rsa.pub /home/vagrant/.ssh/
+  sudo cat /home/vagrant/.ssh/ubuntu1_id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+  sudo cat /home/vagrant/.ssh/ubuntu2_id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+  sudo cat /home/vagrant/.ssh/ubuntu3_id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 }) &
